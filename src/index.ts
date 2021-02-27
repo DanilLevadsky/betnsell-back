24e3e82f2455928cfd5fb2b8093f75aa8da8a742
Danil Levadsky <danillevadsky@gmail.com>
2021-02-27 21:27:51 +0200

    eslint set up

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-import * as http from 'http';
+import * as http from "http";
 
-const host = 'localhost';
+const host = "localhost";
 const port = 8000;
 
 
-const requestListener = function (request: http.IncomingMessage, response: http.ServerResponse) {
+const requestListener =
+function(request: http.IncomingMessage, response: http.ServerResponse) {
     response.writeHead(200);
-    response.end('first server');
-} ;
+    response.end("first server");
+};
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
     console.log(`server is running on http://${host}:${port}`);
-})
+});
+
