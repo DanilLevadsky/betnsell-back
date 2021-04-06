43bff5536319a7ccec901390c542c7d008fc9dcf
Danil Levadsky <danillevadsky@gmail.com>
2021-04-06 13:48:32 +0300

    create model for User and Product

--- dist/constants/serverConstants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PORT = exports.HOST = void 0;
-exports.HOST = "localhost";
-exports.PORT = 8080;
--- src/db/models/User.ts
+++ src/db/models/User.ts
@@ -1,9 +1,37 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Length, IsEmail, IsMobilePhone, Min, MaxLength } from "class-validator";
 
 @Entity()
 export class User {
 
 	@PrimaryGeneratedColumn()
 	id: number;
+
+	@Column({ nullable: false, unique: true })
+	@Length(4, 32)
+	username: string;
+
+	@Column({ nullable: false, unique: true })
+	@IsEmail()
+	mail: string;
+
+	@Column({ nullable: false })
+	password: string;
+
+	@Column({ nullable: false, unique: true })
+	@MaxLength(12)
+	@IsMobilePhone()
+	phone: string
+
+	@Column({ default: 0 })
+	@Min(0)
+	balance: number;
+
+	@Column()
+	@MaxLength(32)
+	name: string
+
+	@Column()
+	profilePic: string;
 
 }
