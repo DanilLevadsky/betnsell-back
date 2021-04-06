89c809c63d175db29665e034004d43fdfb38253d
Danil Levadsky <danillevadsky@gmail.com>
2021-04-06 22:48:02 +0300

    added db configs to constant files

---
+++ src/constants/configConstants.ts
@@ -0,0 +1,10 @@
+export const HOST: string = "localhost";
+export const PORT: number = 3000;
+
+export const db = {
+	"hostname": "betnsell-db.ckn4r5hx1u4x.eu-central-1.rds.amazonaws.com",
+	"port": 5432,
+	"username": "master",
+	"password": "qwerty123",
+	"db_name": "postgres",
+};
