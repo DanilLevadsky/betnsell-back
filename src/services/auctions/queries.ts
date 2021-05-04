dbb789c866ccb44b3834c4ab0e39472d5462d5a2
Shiron <shiron.kaz@gmail.com>
2021-05-04 20:13:30 +0300

    Improved codestyle in queries.

--- src/services/auctions/queries.ts
+++ src/services/auctions/queries.ts
@@ -10,13 +10,13 @@ const createAuction = async function (data: any) {
 	});
 	await prisma.auction.update({
 		data: {
-			lotExpireDate: new Date(Auction.createdAt.setDate(Auction.createdAt.getDate() + 14))
+			lotExpireDate: new Date(Auction.createdAt.setDate(Auction.createdAt.getDate() + 14)),
 		},
 		where: {
-			id: Auction.id
-		}
-	})
-	return getAuctionById(Auction.id)
+			id: Auction.id,
+		},
+	});
+	return getAuctionById(Auction.id);
 };
 
 const getAuctionById = async function (id: number) {
